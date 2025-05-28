7fe4b3750fe28e33ddf415fc4283ef5e0106a7ba
Krzysztof Głowacz <glowicz10@gmail.com>
2025-05-28 21:18:30 +0200

    Zadanie 2.2

--- matura-maj-2025/zad_2.cpp
+++ matura-maj-2025/zad_2.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+const int LICZBA_LINII = 2000;
+const int LICZBA_KOLUMN = 12;
+
 ifstream fin;
 ofstream fout;
 
@@ -20,7 +23,7 @@ void zadanie1() {
     fout.open("./wyniki/wyniki2.txt");
     fout << "2.1." << endl;
     string line;
-    for (int i = 0; i < 2000; i++) {
+    for (int i = 0; i < LICZBA_LINII; i++) {
         fin >> line;
         if (czyPalindrom(line))
             fout << line << endl;
@@ -34,9 +37,67 @@ void zadanie1() {
 }
 
 
+bool czySrodekKwadratu(string siatka[LICZBA_LINII], int wiersz, int kolumna) {
+    if (wiersz == 0 || kolumna == 0 || wiersz == LICZBA_LINII - 1 || kolumna == LICZBA_KOLUMN - 1)
+        return false;
+    if (siatka[wiersz - 1][kolumna - 1] != siatka[wiersz][kolumna])
+        return false;
+    if (siatka[wiersz - 1][kolumna] != siatka[wiersz][kolumna])
+        return false;
+    if (siatka[wiersz - 1][kolumna + 1] != siatka[wiersz][kolumna])
+        return false;
+    if (siatka[wiersz][kolumna - 1] != siatka[wiersz][kolumna])
+        return false;
+    if (siatka[wiersz][kolumna + 1] != siatka[wiersz][kolumna])
+        return false;
+    if (siatka[wiersz + 1][kolumna - 1] != siatka[wiersz][kolumna])
+        return false;
+    if (siatka[wiersz + 1][kolumna] != siatka[wiersz][kolumna])
+        return false;
+    if (siatka[wiersz + 1][kolumna + 1] != siatka[wiersz][kolumna])
+        return false;
+
+    return true;
+}
+
+
+void zadanie2() {
+    fin.open("./dane/symbole.txt");
+    fout.open("./wyniki/wyniki2.txt");
+    fout << "2.2." << endl;
+
+    string siatka[LICZBA_LINII];
+    for (int i = 0; i < LICZBA_LINII; i++) {
+        fin >> siatka[i];
+    }
+
+    int licznik = 0;
+    for (int i = 0; i < LICZBA_LINII; i++) {
+        for (int j = 0; j < LICZBA_KOLUMN; j++) {
+            if (czySrodekKwadratu(siatka, i, j)) {
+                licznik++;
+            }
+        }
+    }
+    fout << licznik;
+    for (int i = 0; i < LICZBA_LINII; i++) {
+        for (int j = 0; j < LICZBA_KOLUMN; j++) {
+            if (czySrodekKwadratu(siatka, i, j)) {
+                fout << " " << i + 1 << " " << j + 1;
+            }
+        }
+    }
+    fout << "\n";
+
+    fin.close();
+    fout.close();
+}
+
+
 int main() {
 
-    zadanie1();
+    // zadanie1();
+    zadanie2();
 
     return 0;
 }
