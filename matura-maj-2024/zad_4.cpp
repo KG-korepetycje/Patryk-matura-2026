20fc186b359bedbde12a54ceb4d72c6f05b2d65f
Krzysztof Głowacz <glowicz10@gmail.com>
2025-07-11 11:43:57 +0200

    Zadanie 4.3 i 4.4 (poczatek) z matury maj 2024

--- matura-maj-2024/zad_4.cpp
+++ matura-maj-2024/zad_4.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-const string PLIK_WEJSCIOWY = "./dane/liczby.txt";
+const string PLIK_WEJSCIOWY = "./dane/liczby_przyklad.txt";
 const string PLIK_WYJSCIOWY = "./wyniki/wyniki4.txt";
-const int LICZBA_LICZB_PIERWSZY_WIERSZ = 3000;
+const int LICZBA_LICZB_PIERWSZY_WIERSZ = 200;
 const int LICZBA_LICZB_DRUGI_WIERSZ = 20;
 
 ifstream fin;
@@ -75,11 +75,108 @@ void zadanie4_2() {
 }
 
 
+bool czy_mozliwy_rozklad(int n, map<int, int> czynniki_pierwsze_plik) {
+    map<int, int> rozklad_liczby;
+
+    int dzielnik = 2;
+    // cout << "Rozklad dla liczby: " << n << endl;
+    while (n > 1) {
+        if (n % dzielnik == 0) {
+            if (rozklad_liczby.count(dzielnik) == 0)  // Sprawdzamy, czy dzielnik byl dodany do mapy
+                rozklad_liczby[dzielnik] = 1;  // Nie byl
+            else
+                rozklad_liczby[dzielnik]++;
+            n = n / dzielnik;
+        }
+        else
+            dzielnik++;
+    }
+
+    // for (pair<int, int> klucz_wartosc : rozklad_liczby) {
+    //     cout << klucz_wartosc.first << " -> " << klucz_wartosc.second << endl;
+    // }
+
+    for (pair<int, int> dzielnik_licznik : rozklad_liczby) {
+        if (czynniki_pierwsze_plik.count(dzielnik_licznik.first) == 0)  // Sprawdzamy, czy dany dzielnik w ogole wystapil w pliku
+            return false;
+        if (czynniki_pierwsze_plik[dzielnik_licznik.first] < dzielnik_licznik.second)
+            return false;
+    }
+
+    return true;
+}
+
+
+void zadanie4_3() {
+
+    fin.open(PLIK_WEJSCIOWY);
+    fout << "4.3." << endl;
+
+    map<int, int> czynniki_pierwsze_plik;
+
+    int liczba;
+    for (int i = 0; i < LICZBA_LICZB_PIERWSZY_WIERSZ; i++) {
+        fin >> liczba;
+        if (czynniki_pierwsze_plik.count(liczba) == 0)
+            czynniki_pierwsze_plik[liczba] = 1;
+        else
+            czynniki_pierwsze_plik[liczba]++;
+    }
+
+    // for (pair<int, int> klucz_wartosc : czynniki_pierwsze_plik) {
+    //     cout << "Klucz: " << klucz_wartosc.first << " -> " << klucz_wartosc.second << endl;
+    // }
+
+    for (int i = 0; i < LICZBA_LICZB_DRUGI_WIERSZ; i++) {
+        fin >> liczba;
+        if (czy_mozliwy_rozklad(liczba, czynniki_pierwsze_plik))
+            fout << liczba << endl;
+    }
+    
+    fin.close();
+
+}
+
+
+void zadanie4_4() {
+
+    fin.open(PLIK_WEJSCIOWY);
+    fout << "4.4." << endl;
+    
+    int suma = 0;
+    int liczby[LICZBA_LICZB_PIERWSZY_WIERSZ + 1];
+    for (int i = 0; i < LICZBA_LICZB_PIERWSZY_WIERSZ; i++) {
+        fin >> liczby[i];
+        if (i < 50)
+            suma = suma + liczby[i];
+    }
+
+    int ogon = 0;
+    int glowa = 49;
+    float max_srednia = suma / 50;
+    int max_dlugosc_ciagu = 50;
+    int max_pierwszy_element_ciagu = liczby[0];
+
+    while (ogon <= LICZBA_LICZB_PIERWSZY_WIERSZ - 50) {
+        if (glowa - ogon == 49)
+            glowa++;
+        else {
+            // Trzeba zrobic sprawdzenie, czy bardziej oplaca sie
+            // rozszerzyc glowa czy skrocic ogonem
+        }
+    }
+    
+    fin.close();
+
+}
+
+
 int main() {
     fout.open(PLIK_WYJSCIOWY);
 
     zadanie4_1();
     zadanie4_2();
+    zadanie4_3();
 
     fout.close();
 
